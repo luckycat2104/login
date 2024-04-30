dca6f6d453f2b1f0349cfe4b734f209b89165bda
LARA MELLO SONZA <lara_sonza@estudante.sesisenai.org.br>
2024-04-30 11:59:08 -0300

    1.0

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './login';
-import { Home } from './home';
-import {Cadastro} from './cadastro';
-
+import Login from './pages/login';
+import Cadastro from './pages/cadastro';
+import Home from './pages/home';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} /> 
-        <Route path="/home" element={<Home />} />        
+        <Route path="/" element={<Login />} />        
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
--- src/home.jsx
+++
@@ -1,8 +0,0 @@
-function Home(){
-return(
-    <h1>
-        Ta na mão pai
-    </h1>
-)
-}
-export {Home}
